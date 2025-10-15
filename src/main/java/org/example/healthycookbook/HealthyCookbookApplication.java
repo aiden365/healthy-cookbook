d4c732cf576ee6a672c9d493eb23518adc524484
chenjiacheng1 <chenjiacheng365@qq.com>
2025-10-15 16:31:57 +0800

    初始提交

---
+++ src/main/java/org/example/healthycookbook/HealthyCookbookApplication.java
@@ -0,0 +1,13 @@
+package org.example.healthycookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyCookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyCookbookApplication.class, args);
+    }
+
+}
